e71d3ee1749fc78b217f83a7978d8d2e0ffc93bf
Armin Dervisefendic <armindervisefendic@gmail.com>
2025-01-10 15:12:08 +0100

    comments

--- backend/src/main/java/com/example/demo/service/DatabaseService.java
+++ backend/src/main/java/com/example/demo/service/DatabaseService.java
@@ -13,6 +13,11 @@ public class DatabaseService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public List<Map<String, Object>> getTables() { // debugging --- delete later
+        String query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name";
+        return jdbcTemplate.queryForList(query);
+    }
+
     public List<Map<String, Object>> executeQuery(String query) {
         return jdbcTemplate.queryForList(query);
     }
