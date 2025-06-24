11e9ef5013a94ae71cd6fa1c96ea9ba471b158b3
Armin Dervisefendic <armindervisefendic@gmail.com>
2025-06-24 20:39:20 +0200

    css + new imgs

--- react-app/src/pages/Welcome.jsx
+++ react-app/src/pages/Welcome.jsx
@@ -6,16 +6,14 @@ const Welcome = () => {
         <div className="welcome-main">
             <div className="welcome-text">
                 <h1>Welcome to eHealth Insights</h1>
-                <p>Welcome to the eHealth Insights, your innovative eHealth Data Space. 
-                    Our platform is designed to securely manage and analyze health data, 
-                    ensuring a seamless and secure exchange of healthcare information 
+                <p>Welcome to the eHealth Insights, your innovative eHealth Data Space.
+                    Our platform is designed to securely manage and analyze health data,
+                    ensuring a seamless and secure exchange of healthcare information
                     while maintaining the highest standards of privacy and security</p>
             </div>
-            <div className="welcome-image-full">
-                <img src="/src/img/Image1.png" alt="Description of image 1" />
-            </div>
+
             <div className="welcome-images-side-by-side">
-                <img src="/src/img/Image2.png" alt="Description of image 2" />
+                <img src="/src/img/Image1.png" alt="Description of image 1" />
                 <img src="/src/img/Image3.png" alt="Description of image 3" />
             </div>
         </div>
